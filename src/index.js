ebe642bce4fbb108fc91b5847e53bee4004c6f8e
Vera Weikel <vera.weikel@gmail.com>
2023-02-07 11:32:04 -0800

    Updates file for dino api pulling.
    Co-authored-by: Jannon Sielaff <jannon.sielaff@gmail.com>
    Co-authored by: Molly Donegan <mollyrdonegan@gmail.com>
    Co-authored by: Molly Donegan <lee.justin001126@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,41 +2,72 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-// Business Logic
+// Business Logic+
 
-function getWeather(city) {
+function getDino() {
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+  const url = `https://dinoipsum.com/api/?format=json&words=10&paragraphs=3`;
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
+    let array1 = this.reponse;
+    console.log(array1);
+    printElements(response);
+    
   });
 
   request.open("GET", url, true);
   request.send();
 }
 
+function printElements(fish) {
+  console.log(fish);
+  document.querySelector('#showResponse').innerText = fish;
+}
+
+// addNumbers(x,y) {
+//   let z = x + y;
+//   return z 
+// }
+
+// let text1 = "Hello world"
+// let text2 = "Nice to meet you"
+
+// let result = addNumbers(text1,text2)
+// console.log(result)
+
+
+// result = "Hello worldNice to meet you"
+// function getWeather(city) {
+//   let request = new XMLHttpRequest();
+//   const url = `https://dinoipsum.com/api/?format=html&words=5&paragraphs=2`;
+
+// //https://dinoipsum.com/api/?format=html&words=10&paragraphs=3
+  
+//   request.addEventListener("loadend", function() {
+//     const response = JSON.parse(this.responseText);
+//     if (this.status === 200) {
+//       printElements(response, city);
+//     } else {
+//       printError(this, response, city);
+//     }
+//   });
+
+//   request.open("GET", url, true);
+//   request.send();
+// }
+
 // UI Logic
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. 
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
-}
-
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
-}
+// function printError(request, apiResponse, city) {
+//   document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+// }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getWeather(city);
+  // const city = document.querySelector('#location').value;
+  // document.querySelector('#location').value = null;
+  getDino();
 }
 
 window.addEventListener("load", function() {
