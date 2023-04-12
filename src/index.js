f2690e8517266c4f65b1844f1a1579ec80ee5cee
Vera Weikel <vera.weikel@gmail.com>
2023-04-12 11:37:35 -0700

    Index.js: create role playing game instance.
    Jennifer Holcomb <jenniferlholcomb@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,98 +3,121 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 
 const storeState = () => {
-  let currentState = {};
-  return (stateChangeFunction = (state) => state) => {
+  let currentState = {}; 
+  return (stateChangeFunction = (state) => state => {
     const newState = stateChangeFunction(currentState);
     currentState = { ...newState };
     return newState;
-  };
+  });
 };
 
-const stateControl = storeState();
-
-const changeState = (prop) => {
+const stateChange = (prop) => {
   return (value) => {
-    return (state) => (
-      {
-        ...state,
-        [prop]: (state[prop] || 0) + value,
-      });
+    return (state) => ({
+      ...state, 
+      [prop]: (state[prop] || 0) + value 
+    });
   };
 };
 
+window.onload = function () {
 
-const plant0 = storeState({ soil: 0, water: 0, light: 0 });
-// const plant0 =("soil")(14) {...storeState(changeState("soil")(14) )}; //stateChangeFunction
+  const stateControl = storeState();
+  
+  document.getElementById("create-character").onsubmit = function (event) {
+    event.preventDefault();
+    let characterName = document.getElementById("character-name").value;
+    const character = stateControl(changeState("name")(characterName));
+    document.getElementById(
+      "name"
+    ).innerText = `Character Name: ${character.name}`;
+  };
+};
 
-const feed = changeState("soil")(1);
-const blueFood = changeState("soil")(5);
+// const storeState = (initialState) => { //
+//   let currentState = {initialState}; // {};
+//   return (stateChangeFunction = (state) => state) => {
+//     const newState = stateChangeFunction(currentState);
+//     currentState = { ...newState };
+//     return newState;
+//   };
+// };
 
-const hydrate = changeState("water")(1);
-const superWater = changeState("water")(5);
+// const changeState = (prop) => {
+//   return (value) => {
+//     return (state) => (
+//       {
+//         ...state,
+//         [prop]: (state[prop] || 0) + value,
+//       });
+//   };
+// };
 
-const sunshine = changeState("light")(111);
-const plantLamp = changeState("light")(5);
- 
+// const plant0 = storeState({ soil: 0, water: 0, light: 0 });
+// // const plant0 =("soil")(14) {...storeState(changeState("soil")(14) )}; //stateChangeFunction
 
-console.log(
-  "feed, blueFood, hydrate, superWater, sunshine, plantLamp",
-  feed,
-  blueFood,
-  hydrate,
-  superWater,
-  sunshine,
-  plantLamp
-);
+// const feed = changeState("soil")(1);
+// const blueFood = changeState("soil")(5);
+
+// const hydrate = changeState("water")(1);
+// const superWater = changeState("water")(5);
+
+// const sunshine = changeState("light")(111);
+// const plantLamp = changeState("light")(5);
+
 // console.log(
-//   "blueFood, sunshine, plantLamp",
+//   "feed, blueFood, hydrate, superWater, sunshine, plantLamp",
+//   feed,
 //   blueFood,
+//   hydrate,
+//   superWater,
 //   sunshine,
 //   plantLamp
 // );
-window.onload = function () {
-  document.getElementById("feed").onclick = function () {
-    const newState = stateControl(blueFood);
-    
-    
-    document.getElementById("soil-value").innerText = `Soil: ${newState.soil}`;
-  };
 
-  document.getElementById("feedPlant0").onclick = function () {
-    const plant0Feed = plant0(blueFood);
-    console.log("Plant Zero:", plant0Feed);
-    document.getElementById(
-      "soil1-value"
-    ).innerText = `Soil Plant Zero: ${plant0Feed.soil}`;
-  };
+// window.onload = function () {
 
-  document.getElementById("show-state").onclick = function () {
-    const currentState = stateControl();
-    document.getElementById(
-      "soil-value"
-    ).innerText = `Soil: ${currentState.soil}`;
-  };
+//   // const stateControl = storeState(); // fx storeState(currentState = {}; );
+//   const stateControl = storeState({}); // (a.k.a. storeState(initialState); // arr[1,2,x];
 
-  // document.getElementById("show-state").onclick = function () {
-  //   const currentState = stateControl(); // { ...stateControl("soil")(4)};// plant0(); // plant0Feed(); 
-  //   document.getElementById(
-  //     "soil1-value"
-  //   ).innerText = `Soil: ${currentState.soil}`;
-  // };
+//   const multPlants = [];
 
-  // // document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+//   document.getElementById("feed").onclick = function () {
+//     const newState = stateControl(blueFood);
+//     document.getElementById("soil-value").innerText = `Soil: ${newState.soil}`;
+//   };
 
-  // document.getElementById("soil-value").innerText = `Soil: ${blueFood.soil}`;
-  // document.getElementById("show-state").onclick = function () {
-  //   const currentState = stateControl();
-  //   document.getElementById(
-  //     "soil-value"
-  //   ).innerText = `Soil:  ${currentState.soil}`; // ${blueFood.soil}`, `currentSt:
-  // };
-};
+//   document.getElementById("feedPlant0").onclick = function () {
+//     const plant0Feed = plant0(blueFood);
+//     console.log("Plant Zero:", plant0Feed);
+//     document.getElementById(
+//       "soil1-value"
+//     ).innerText = `Soil Plant Zero: ${plant0Feed.soil}`;
+//   };
 
-//document.querySelector("form")
-//   .addEventListener("submit", handleFormSubmission);
-// stateControl();
-// }
-// }
+//   document.getElementById("show-state").onclick = function () {
+//     const currentState = stateControl();
+//     document.getElementById(
+//       "soil-value"
+//     ).innerText = `Soil: ${currentState.soil}`;
+//     multPlants.push(currentState);
+//     console.log(multPlants);
+//   };
+
+// document.getElementById("show-state").onclick = function () {
+//   const currentState = stateControl(); // { ...stateControl("soil")(4)};// plant0(); // plant0Feed();
+//   document.getElementById(
+//     "soil1-value"
+//   ).innerText = `Soil: ${currentState.soil}`;
+// };
+
+// // document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+
+// document.getElementById("soil-value").innerText = `Soil: ${blueFood.soil}`;
+// document.getElementById("show-state").onclick = function () {
+//   const currentState = stateControl();
+//   document.getElementById(
+//     "soil-value"
+//   ).innerText = `Soil:  ${currentState.soil}`; // ${blueFood.soil}`, `currentSt:
+// };
+// };
